67cb932ea87091c6174e3e7ed35bef24dbd12e63
Prakash <akshav00@gmail.com>
2017-08-21 09:54:38 -0700

    url

--- app/src/main/java/com/example/icubeapp/common/GlobalClass.java
+++ app/src/main/java/com/example/icubeapp/common/GlobalClass.java
@@ -28,8 +28,8 @@ public class GlobalClass extends Application {
     public static ArrayList<FeedBackSelection> feedbackdata=new ArrayList<>();
     public static ArrayList<Slider> data=new ArrayList<>();
     public static int position=0;
-    public static String globalurl="http://192.168.10.13:81";
-   // public static String globalurl="http://icube.cloudapp.net:8080/iCubeIOS/";
+    //public static String globalurl="http://192.168.10.13:81";
+    public static String globalurl="http://icube.cloudapp.net:8080/iCubeIOS";
     public void onCreate() {
 
 
